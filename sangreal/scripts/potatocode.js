e99163a5fdfcf540f4a8519bc165bbd84f44c131
Roman <61108970+Uwkee@users.noreply.github.com>
2021-01-20 11:35:34 +0400

    Update potatocode.js

--- sangreal/scripts/potatocode.js
+++ sangreal/scripts/potatocode.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //document.querySelector('.pg-content').innerHTML = '<object type="text/html" data="/tmp.html"></object>';
+    document.querySelector('.pg-content').onclick = function() {
+        fetch('/test.html')
+        .then(data => data.text())
+        .then(html => document.querySelector('.pg-content').innerHTML = html);
+    };
 });
