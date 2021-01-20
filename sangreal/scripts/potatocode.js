96052a1f6c6464f619318a9d123e2c45b513d0c3
Roman <61108970+Uwkee@users.noreply.github.com>
2021-01-20 11:43:14 +0400

    Update potatocode.js

--- sangreal/scripts/potatocode.js
+++ sangreal/scripts/potatocode.js
@@ -1,7 +1,2 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.pg-content').onclick = function() {
-        fetch('https://uwkee.github.io/sangreal/test.html/')
-        .then(data => data.text())
-        .then(html => document.querySelector('.pg-content').innerHTML = html);
-    };
 });
